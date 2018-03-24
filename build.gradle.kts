2f42914bc80d89dae61216c8ed58b95d979d5aa4
Ben Chatelain <ben@octop.ad>
2018-03-23 22:03:20 -0600

    ➕ Add plugins
    - bintray 1.8.0
    - plugin-publish 0.9.10
    - java-gradle-plugin
    - maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,7 @@
 
 import org.gradle.api.JavaVersion.*
 import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.include
-import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.extra
-import org.gradle.kotlin.dsl.get
-import org.gradle.kotlin.dsl.repositories
+import org.gradle.kotlin.dsl.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.junit.platform.console.options.Details
 import org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns
@@ -22,7 +19,16 @@ import org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatte
 /* -------------------------------------------------------------------------- */
 
 plugins {
+    // Gradle built-in
+    `java-gradle-plugin`
+    `maven-publish`
+
+    // Gradle plugin portal - https://plugins.gradle.org/
+    id("com.jfrog.bintray") version "1.8.0"
+    id("com.gradle.plugin-publish") version "0.9.10"
     id("org.jetbrains.kotlin.jvm") version "1.2.30"
+
+    // Custom handling in pluginManagement
     id("org.junit.platform.gradle.plugin") version "1.1.0"
 }
 
@@ -30,8 +36,12 @@ plugins {
 // 📋 Properties
 /* -------------------------------------------------------------------------- */
 
+val artifactName by project
+val javaPackage = "$group.$artifactName"
+val pluginClass by project
+
 val jvmTarget = JavaVersion.VERSION_1_8.toString()
-val spekVersion = "1.1.5"
+val spekVersion by project
 
 // This is necessary to make the plugin version accessible in other places
 // https://stackoverflow.com/questions/46053522/how-to-get-ext-variables-into-plugins-block-in-build-gradle-kts/47507441#47507441
@@ -75,6 +85,61 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = jvmTarget
 }
 
+// Include resources
+java.sourceSets["main"].resources {
+    setSrcDirs(mutableListOf("src/main/resources"))
+    include("VERSION.txt")
+}
+
+val updateVersionFile by tasks.creating {
+    description = "Updates the VERSION.txt file included with the plugin"
+    group = "Build"
+    doLast {
+        project.file("src/main/resources/VERSION.txt").writeText(version.toString())
+    }
+}
+
+tasks.getByName("assemble").dependsOn(updateVersionFile)
+
+val sourcesJar by tasks.creating(Jar::class) {
+    dependsOn("classes")
+    classifier = "sources"
+    from(java.sourceSets["main"].allSource)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    dependsOn("javadoc")
+    classifier = "javadoc"
+    val javadoc = tasks.withType<Javadoc>().first()
+    from(javadoc.destinationDir)
+}
+
+artifacts.add("archives", sourcesJar)
+artifacts.add("archives", javadocJar)
+
+configure<BasePluginConvention> {
+    // at.phatbl.tube-1.0.0.jar
+    archivesBaseName = javaPackage
+}
+
+/*gradlePlugin.plugins.create(artifactName) {
+    id = javaPackage
+    implementationClass = "$javaPackage.$pluginClass"
+}
+
+pluginBundle {
+    website = "https://github.com/phatblat/ShellExec"
+    vcsUrl = "https://github.com/phatblat/ShellExec"
+    description = "Exec base task alternative which runs commands in a Bash shell."
+    tags = mutableListOf("gradle", "exec", "shell", "bash", "kotlin")
+
+    plugins.create("shellexec") {
+        id = javaPackage
+        displayName = "ShellExec plugin"
+    }
+    mavenCoordinates.artifactId = artifactName
+}*/
+
 /* -------------------------------------------------------------------------- */
 // ✅ Test
 /* -------------------------------------------------------------------------- */
@@ -84,6 +149,64 @@ junitPlatform {
         engines {
             include("spek")
         }
+        includeClassNamePatterns("^.*Tests?$", ".*Spec", ".*Spek")
     }
     details = Details.TREE
+}
+
+/* -------------------------------------------------------------------------- */
+// 🚀 Deployment
+/* -------------------------------------------------------------------------- */
+/*
+publishing {
+    (publications) {
+        "mavenJava"(MavenPublication::class) {
+            from(components["java"])
+            artifactId = artifactName
+
+            artifact(sourcesJar) { classifier = "sources" }
+            artifact(javadocJar) { classifier = "javadoc" }
+        }
+    }
+}
+
+bintray {
+    user = property("bintray.user") as String
+    key = property("bintray.api.key") as String
+    setPublications("mavenJava")
+    setConfigurations("archives")
+    dryRun = false
+    publish = true
+    pkg.apply {
+        repo = "maven-open-source"
+        name = "ShellExec"
+        desc = "Gradle plugin with a simpler Exec task."
+        websiteUrl = "https://github.com/phatblat/ShellExec"
+        issueTrackerUrl = "https://github.com/phatblat/ShellExec/issues"
+        vcsUrl = "https://github.com/phatblat/ShellExec.git"
+        setLicenses("MIT")
+        setLabels("gradle", "plugin", "exec", "shell", "bash")
+        publicDownloadNumbers = true
+        version.apply {
+            name = project.version.toString()
+            desc = "ShellExec Gradle Plugin ${project.version}"
+            released = Date().toString()
+            vcsTag = project.version.toString()
+            attributes = mapOf("gradle-plugin" to "${project.group}:$artifactName:$version")
+
+            mavenCentralSync.apply {
+                sync = false //Optional (true by default). Determines whether to sync the version to Maven Central.
+                user = "userToken" //OSS user token
+                password = "password" //OSS user password
+                close = "1" //Optional property. By default the staging repository is closed and artifacts are released to Maven Central. You can optionally turn this behaviour off (by puting 0 as value) and release the version manually.
+            }
+        }
+    }
+}*/
+
+val deploy by tasks.creating {
+    description = "Deploys the artifact."
+    group = "Deployment"
+    dependsOn("bintrayUpload")
+    dependsOn("publishPlugins")
 }
